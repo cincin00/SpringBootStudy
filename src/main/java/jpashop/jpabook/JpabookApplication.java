7d7a9243e43fcfc5def386d927039b500e3c7534
changon.you <dbcksrhs30@yahoo.co.kr>
2022-09-28 17:09:32 +0900

    Week 3 Lesson commit

--- src/main/java/jpashop/jpabook/JpabookApplication.java
+++ src/main/java/jpashop/jpabook/JpabookApplication.java
@@ -6,10 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpabookApplication {
 	 public static void main(String[] args){
+		// 예제1. 데이터 출력
 		Hello hello = new Hello();
 		hello.setData("gabiacns BootSpring Study");
 		String data = hello.getData();
 		System.out.println("data::::"+data);
+		// 예제2. 테이블 세팅
+		//Member member = new Member();
+
 		SpringApplication.run(JpabookApplication.class, args);
 	}
 
